e08d4ed572643c84bcab87802030d347b61aa9b0
Kumar Swamy Akkyana <kumar.swamy@neosoftmail.com>
2022-09-22 18:32:02 +0530

    first alottiejs

---
+++ index.js
@@ -0,0 +1,9 @@
+let animation = bodymovin.loadAnimation({
+
+    container: document.getElementById('container'), 
+    path: 'fall-login.json',
+    renderer: 'svg', 
+    loop: true, 
+    autoplay: true, 
+    
+    });
